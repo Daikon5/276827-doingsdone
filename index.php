201c4ee64b721eaeb8b80ec7e339feea571f59f3
Daikon5 <fiveman123@gmail.com>
2017-11-09 22:59:19 +0600

    Костыли
    То что требуется по заданию работает, но сделано так себе, как по мне.
    Чекбокс я вообще не понимаю как работает.

--- index.php
+++ index.php
@@ -108,7 +108,13 @@ $days_until_deadline = null;
                     <label class="checkbox">
                         <a href="/">
                             <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                            <input class="checkbox__input visually-hidden" type="checkbox">
+                            <?
+                            $checked = false;
+                            if ($show_complete_tasks == 1) {
+                              $checked = true;
+                            }
+                            ?>
+                            <input class="checkbox__input visually-hidden" type="checkbox" <?=($show_complete_tasks == 1) ? 'checked="checked"' : '' ?>>
                             <span class="checkbox__text">Показывать выполненные</span>
                         </a>
                     </label>
@@ -116,6 +122,20 @@ $days_until_deadline = null;
 
                 <table class="tasks">
                     <!--показывать следующий тег <tr/>, если переменная равна единице-->
+                    <? if ($show_complete_tasks == 1): ?>
+                    <tr class="tasks__item task task--completed">
+                      <td class="task__select">
+                        <label class="checkbox task__checkbox">
+                          <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                          <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                        </label>
+                      </td>
+                      <td class="task__date">10.04.2017</td>
+
+                      <td class="task__controls">
+                      </td>
+                    </tr>
+
                     <tr class="tasks__item task task--completed">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
@@ -145,6 +165,37 @@ $days_until_deadline = null;
 
                         <td class="task__date">21.03.2017</td>
                     </tr>
+                    <? else: ?>
+                    <tr class="tasks__item task task--completed">
+                        <td class="task__select">
+                            <label class="checkbox task__checkbox">
+                                <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                <a href="/"><span class="checkbox__text">Сделать главную страницу Дела в порядке</span></a>
+                            </label>
+
+                        </td>
+
+                        <td class="task__file">
+                            <a class="download-link" href="#">Home.psd</a>
+                        </td>
+
+                        <td class="task__date"><!--выведите здесь дату выполнения задачи--></td>
+                    </tr>
+
+                    <tr class="tasks__item task">
+                        <td class="task__select">
+                            <label class="checkbox task__checkbox">
+                                <input class="checkbox__input visually-hidden" type="checkbox">
+                                <a href="/"><span class="checkbox__text">Выполнить домашнее задание</span></a>
+                            </label>
+                        </td>
+
+                        <td class="task__file">
+                        </td>
+
+                        <td class="task__date">21.03.2017</td>
+                    </tr>
+                  <? endif; ?>
                 </table>
             </main>
         </div>
