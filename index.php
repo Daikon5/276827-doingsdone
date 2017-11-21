82259fe82e6cc04f332da22e9e474e24d2326bc5
Daikon5 <fiveman123@gmail.com>
2017-11-21 14:14:20 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once "data.php";
-require_once "functions.php";
+include "data.php";
+include "functions.php";
 
 $page_content = render_page("templates/index.php", ["tasks" => $tasks, "sct" => $show_complete_tasks]);
 $layout_content = render_page("templates/layout.php", ["title" => $title, "categories" => $categories, "content" => $page_content, "tasks" => $tasks]);
