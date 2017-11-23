f469bb07595d08c9b861a55c8ef834acf23630ed
Daikon5 <fiveman123@gmail.com>
2017-11-23 20:18:28 +0600

    1

--- index.php
+++ index.php
@@ -2,6 +2,36 @@
 include "data.php";
 include "functions.php";
 
+$category = null;
+$tasks_chosen = [];
+$counter = 0;
+if (isset($_GET["category"])) {
+    $category = $_GET["category"];
+
+    if (isset($categories[$category])) {
+        if ($category == 0) {                           //если категория "Все"
+            $tasks_chosen = $tasks;
+        }
+        else {
+            foreach ($tasks as $task) {
+                if ($categories[$category] == $task["cat"]) {
+                    array_push($tasks_chosen,$task);
+                }
+                else {
+                    $counter++;
+                    if ($counter == count($categories)) {  //если в выбранной категории 0 проектов
+                        $tasks_chosen = $tasks;
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (!$category) {
+http_response_code (404);
+}
+
 $page_content = render_page("templates/index.php", ["tasks" => $tasks, "sct" => $show_complete_tasks]);
 $layout_content = render_page("templates/layout.php", ["title" => $title, "categories" => $categories, "content" => $page_content, "tasks" => $tasks]);
 
